f3cadc36c79607079a68a943310ea531bbecebb8
João Petinga <joao@juicee.io>
2019-11-30 11:34:06 +0000

    add vue snippet for component

---
+++ src/views/About2.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>About page, hello!</h1>
+</template>
+<script>
+export default {
+  components: {},
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
+<style scoped></style>
